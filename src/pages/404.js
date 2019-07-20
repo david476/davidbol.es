37a3f67596aac260fc7859e969d19d9c9d25cce4
David Boles <me@davidbol.es>
2019-07-20 12:10:26 -0700

    Prep

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default () => (
+  <div>
+    <h1>404 - If this is unexpected, please submit an issue or contact me at <a href="mailto:me@davidbol.es">me@davidbol.es</a></h1>
+  </div>
+)
